91c79b6bd0e9c0d02dbd30ca6a95c913cbb9777d
hieuvovan <hieuvo0904@gmail.com>
2022-12-05 09:44:28 +0700

    implement UI

---
+++ client/src/constants/index.js
@@ -0,0 +1,3 @@
+export const QUERY = {
+  TRACKS_FOR_HOME: "tracksForHome",
+};
--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql, useQuery } from "@apollo/client";
+import { QUERY } from "../constants";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+const TRACKS = gql`
+  query GetTracks {
+    ${QUERY.TRACKS_FOR_HOME} {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult {...{ error, loading, data }}>
+        {data?.[QUERY.TRACKS_FOR_HOME]?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}x
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
