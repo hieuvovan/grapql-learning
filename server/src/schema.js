a305c862c41447621551c7bf998f6424cd131d7c
hieuvovan <hieuvo0904@gmail.com>
2022-12-05 09:40:00 +0700

    complete project demo

---
+++ server/src/schema.js
@@ -0,0 +1,54 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+    track(id: ID!): Track
+  }
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse
+  }
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
+`;
+
+module.exports = typeDefs;
